9feedbcb888193da2cb2b6507500b64c93dc02b3
Nagy Gábor <ngabor0508@gmail.com>
2021-09-29 15:03:14 +0200

    Metódusok létrehozása elkészült.

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,5 +1,47 @@
 package hu.petrik;
 
-public class Bejegyzes {
+import java.time.LocalDateTime;
+import java.util.Date;
 
+public class Bejegyzes {
+    private String szerzo;
+    private  String tartalom;
+    private  int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom){
+        likeok = 0;
+        letrejott = LocalDateTime.now();
+        szerkesztve = LocalDateTime.now();
+
+    }
+
+    public String getSzerzo(){
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+        likeok++;
+    }
 }
